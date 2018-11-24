515793bd2a7a66363041f355943a2fe2f1266d15
root <root@ip-172-31-18-243.us-west-2.compute.internal>
2018-11-24 19:00:59 +0000

    Productionize db url.

--- djangoresume/djangoresume/production.py
+++ djangoresume/djangoresume/production.py
@@ -7,7 +7,7 @@ from .settings import *
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+        default=os.environ.get('DATABASE_URL'))
     }
 
 DEBUG = False
