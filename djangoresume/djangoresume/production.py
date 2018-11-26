8202c2987d9bb6ca8387bbec78f329f02cc42106
root <root@ip-172-31-18-243.us-west-2.compute.internal>
2018-11-26 15:34:58 +0000

    Production updates.

--- djangoresume/djangoresume/production.py
+++ djangoresume/djangoresume/production.py
@@ -12,6 +12,10 @@ DATABASES = {
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+SECURE_SSL_HOST = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
