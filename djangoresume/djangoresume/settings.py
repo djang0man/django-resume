9f87a2cbd482e30ff0eb2c67ed7c9b1ccf63e328
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-12-02 15:58:15 -0800

    Disable api Basic Auth.

--- djangoresume/djangoresume/settings.py
+++ djangoresume/djangoresume/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     ],
